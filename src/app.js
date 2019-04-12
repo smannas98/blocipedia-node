4e06f0bd1838f9001194b356aa72a8bef3a82504
smannas98 <seanmannas02@gmail.com>
2019-04-12 16:32:52 -0400

    made my routes and server files more modular, set up a test in jasmine

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ const appConfig = require('./config/main-config.js');
 
 const app = express();
 
+app.use('/', (req, res, next) => {
+  res.send('this is just a test');
+});
 appConfig.init(app, express);
 routeConfig.init(app);
 
