301676f8b2600fa32669d36420746849846b9d73
smannas98 <seanmannas02@gmail.com>
2019-04-14 11:20:10 -0400

    framed out the index home page for Blocipedia

--- src/app.js
+++ src/app.js
@@ -4,9 +4,6 @@ const appConfig = require('./config/main-config.js');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-  res.send('this is just a test');
-});
 appConfig.init(app, express);
 routeConfig.init(app);
 
